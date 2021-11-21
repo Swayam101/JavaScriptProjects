b6f20af8dba53f466eb6f3d969fbbbbfcc6201b8
Swayam <swayamprajapat21@gmail.com>
2021-11-21 21:45:29 +0530

    Update food.js

--- food.js
+++ food.js
@@ -1,7 +1,7 @@
 import { onSnake,expandSnake } from "./snake.js";
 import { randomGridPosition } from "./grid.js";
 let food = getRandomFoodPosition();
-const EXPANSION_RATE=1;
+const EXPANSION_RATE=3;
 
 
 export function update() {
